a5db5c23693a4fe972ea43463db9f6f0423563fe
Erhard <nielsenofficial0@gmail.com>
2017-09-22 23:31:27 +0000

    update 2

---
+++ that_was_easy/main.js
@@ -0,0 +1,7 @@
+- var thatWasEasy = new Audio("High Frequency - Sound effect.mp3");
+- thatWasEasy.play();
+
++ function sayThatWasEasy() {
++   var thatWasEasy = new Audio("High Frequency - Sound effect.mp3");
++   thatWasEasy.play();
++ }
